63bfb22f6da614fb5d4054f0cb1cf609028eee82
Lucas <ldja@st-andrews.ac.uk>
2019-08-08 19:29:38 +0100

    parcel

--- demo/Demo.js
+++ demo/Demo.js
@@ -1,26 +1,52 @@
-import React from 'react'
+import React from 'react';
 
-import { FilePicker, ImagePicker } from '../lib'
+import { FilePicker } from '../lib';
 
-const Demo = () => (
-  <div>
-    <FilePicker
-      extensions={['md']}
-      onChange={FileObject => console.log(FileObject)}
-      onError={errMsg => console.log(errMsg)}
-    >
-      <button>Click to upload markdown</button>
-    </FilePicker>
-    <br />
-    <ImagePicker
-      extensions={['jpg', 'jpeg', 'png']}
-      dims={{ minWidth: 100, maxWidth: 500, minHeight: 100, maxHeight: 500 }}
-      onChange={base64EncodedString => console.log(base64EncodedString)}
-      onError={errMsg => console.log(errMsg)}
-    >
-      <button>Click to upload image</button>
-    </ImagePicker>
-  </div>
-)
+class Demo extends React.Component {
+  state = {
+    file: {},
+    reset: {},
+  }
+
+
+  render() {
+    const { file } = this.state;
+
+    return (
+      < div className="app" >
+        <header className="App-header">
+          <code>React-Simple-File-Picker</code>
+          <div onClick={() => {
+            this.setState({ reset: Object.assign({}) })
+          }}>clear the picker</div>
+          <FilePicker
+            className="button"
+            maxSize={2}
+            buttonText="Upload a file!"
+            extensions={["application/pdf"]}
+            onChange={(file) => this.setState({ file })}
+            onError={errMsg => { alert("that's an error: " + errMsg) }}
+            onClear={() => this.setState({ file: {} })}
+            triggerReset={this.state.reset}
+          >
+            <div className="input-button" type="button">
+              Dee file picker
+      </div>
+          </FilePicker>
+
+          <div className="file-details">
+            <h3>The file</h3>
+            <h4>Name: {file.name}</h4>
+            <h4>Size: {file.size}{file.size ? ' bytes' : null}</h4>
+            <h4>Type: {file.type}</h4>
+            <h4>Modified: {file.lastModified}</h4>
+
+          </div>
+
+        </header>
+      </div>
+    );
+  }
+}
 
 export default Demo
--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Demo from './Demo'
+import Demo from './Demo';
+import styles from './styles.css';
 
 ReactDOM.render(<Demo />, document.getElementById('root'))
